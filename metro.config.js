d79a603c3769d519fb0ffa803add8b8f94b2ca2c
IngSamuDev <samuelgarcia110106@gmail.com>
2026-04-13 22:57:11 -0500

    Solución de conflictos entre mi rama y la de camila

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
-<<<<<<< HEAD
 
 const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './app/global.css' })
-=======
- 
-const config = getDefaultConfig(__dirname)
- 
 module.exports = withNativeWind(config, { input: '../app/global.css' })
->>>>>>> cami-zapata
